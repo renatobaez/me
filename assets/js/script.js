b2d4997f0790608cd10d9d26df12321a809a93e4
Renato Báez <panchobaez@gmail.com>
2024-11-05 13:37:06 -0300

    sytle black

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
-$(window).load(function(){
-	$('#preloader').fadeOut('slow',function(){$(this).remove();});
+$(window).load(function () {
+	$('#preloader').fadeOut('slow', function () { $(this).remove(); });
 });
 
 
 
-/* Menu */ 
-(function() {
+/* Menu */
+(function () {
 
 	var bodyEl = document.body,
 		//content = document.querySelector( '.content-wrap' ),
-		openbtn = document.getElementById( 'open-button' ),
-		closebtn = document.getElementById( 'close-button' ),
+		openbtn = document.getElementById('open-button'),
+		closebtn = document.getElementById('close-button'),
 		isOpen = false;
 
 	function init() {
@@ -18,18 +18,18 @@ $(window).load(function(){
 	}
 
 	function initEvents() {
-		openbtn.addEventListener( 'click', toggleMenu );
-		if( closebtn ) {
-			closebtn.addEventListener( 'click', toggleMenu );
+		openbtn.addEventListener('click', toggleMenu);
+		if (closebtn) {
+			closebtn.addEventListener('click', toggleMenu);
 		}
 	}
 
 	function toggleMenu() {
-		if( isOpen ) {
-			classie.remove( bodyEl, 'show-menu' );
+		if (isOpen) {
+			classie.remove(bodyEl, 'show-menu');
 		}
 		else {
-			classie.add( bodyEl, 'show-menu' );
+			classie.add(bodyEl, 'show-menu');
 		}
 		isOpen = !isOpen;
 	}
@@ -38,190 +38,190 @@ $(window).load(function(){
 
 })();
 /* smooth scrolling */
-$(document).ready(function(){
-	$("a").on('click', function(event) {
-	  if (this.hash !== "") {
-		
-		event.preventDefault();
-  		var hash = this.hash;
-  		$('html, body').animate({
-		  scrollTop: $(hash).offset().top
-		}, 800, function(){
-  
-		  window.location.hash = hash;
-		});
-	  }
+$(document).ready(function () {
+	$("a").on('click', function (event) {
+		if (this.hash !== "") {
+
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function () {
+
+				window.location.hash = hash;
+			});
+		}
 	});
 });
 
-(function() {
+(function () {
 	"use strict";
-  
+
 	/**
 	 * Easy selector helper function
 	 */
 	const select = (el, all = false) => {
-	  el = el.trim()
-	  if (all) {
-		return [...document.querySelectorAll(el)]
-	  } else {
-		return document.querySelector(el)
-	  }
+		el = el.trim()
+		if (all) {
+			return [...document.querySelectorAll(el)]
+		} else {
+			return document.querySelector(el)
+		}
 	}
-  
+
 	/**
 	 * Easy event listener function
 	 */
 	const on = (type, el, listener, all = false) => {
-	  let selectEl = select(el, all)
-	  if (selectEl) {
-		if (all) {
-		  selectEl.forEach(e => e.addEventListener(type, listener))
-		} else {
-		  selectEl.addEventListener(type, listener)
+		let selectEl = select(el, all)
+		if (selectEl) {
+			if (all) {
+				selectEl.forEach(e => e.addEventListener(type, listener))
+			} else {
+				selectEl.addEventListener(type, listener)
+			}
 		}
-	  }
 	}
-  
+
 	/**
-	 * Easy on scroll event listener 
+	 * Easy on scroll event listener
 	 */
 	const onscroll = (el, listener) => {
-	  el.addEventListener('scroll', listener)
+		el.addEventListener('scroll', listener)
 	}
-  
+
 	/**
 	 * Scrolls to an element with header offset
 	 */
 	const scrollto = (el) => {
-	  let header = select('#header')
-	  let offset = header.offsetHeight
-  
-	  let elementPos = select(el).offsetTop
-	  window.scrollTo({
-		top: elementPos - offset,
-		behavior: 'smooth'
-	  })
+		let header = select('#header')
+		let offset = header.offsetHeight
+
+		let elementPos = select(el).offsetTop
+		window.scrollTo({
+			top: elementPos - offset,
+			behavior: 'smooth'
+		})
 	}
-  
+
 	/**
 	 * Back to top button
 	 */
 	let backtotop = select('.back-to-top')
 	if (backtotop) {
-	  const toggleBacktotop = () => {
-		if (window.scrollY > 100) {
-		  backtotop.classList.add('active')
-		} else {
-		  backtotop.classList.remove('active')
+		const toggleBacktotop = () => {
+			if (window.scrollY > 100) {
+				backtotop.classList.add('active')
+			} else {
+				backtotop.classList.remove('active')
+			}
 		}
-	  }
-	  window.addEventListener('load', toggleBacktotop)
-	  onscroll(document, toggleBacktotop)
+		window.addEventListener('load', toggleBacktotop)
+		onscroll(document, toggleBacktotop)
 	}
-  
+
 	/**
 	 * Mobile nav toggle
 	 */
-	on('click', '.mobile-nav-toggle', function(e) {
-	  select('#navbar').classList.toggle('navbar-mobile')
-	  this.classList.toggle('bi-list')
-	  this.classList.toggle('bi-x')
+	on('click', '.mobile-nav-toggle', function (e) {
+		select('#navbar').classList.toggle('navbar-mobile')
+		this.classList.toggle('bi-list')
+		this.classList.toggle('bi-x')
 	})
-  
+
 	/**
 	 * Mobile nav dropdowns activate
 	 */
-	on('click', '.navbar .dropdown > a', function(e) {
-	  if (select('#navbar').classList.contains('navbar-mobile')) {
-		e.preventDefault()
-		this.nextElementSibling.classList.toggle('dropdown-active')
-	  }
+	on('click', '.navbar .dropdown > a', function (e) {
+		if (select('#navbar').classList.contains('navbar-mobile')) {
+			e.preventDefault()
+			this.nextElementSibling.classList.toggle('dropdown-active')
+		}
 	}, true)
-  
+
 	/**
 	 * Scrool with ofset on links with a class name .scrollto
 	 */
-	on('click', '.scrollto', function(e) {
-	  if (select(this.hash)) {
-		e.preventDefault()
-  
-		let navbar = select('#navbar')
-		if (navbar.classList.contains('navbar-mobile')) {
-		  navbar.classList.remove('navbar-mobile')
-		  let navbarToggle = select('.mobile-nav-toggle')
-		  navbarToggle.classList.toggle('bi-list')
-		  navbarToggle.classList.toggle('bi-x')
+	on('click', '.scrollto', function (e) {
+		if (select(this.hash)) {
+			e.preventDefault()
+
+			let navbar = select('#navbar')
+			if (navbar.classList.contains('navbar-mobile')) {
+				navbar.classList.remove('navbar-mobile')
+				let navbarToggle = select('.mobile-nav-toggle')
+				navbarToggle.classList.toggle('bi-list')
+				navbarToggle.classList.toggle('bi-x')
+			}
+			scrollto(this.hash)
 		}
-		scrollto(this.hash)
-	  }
 	}, true)
-  
+
 	/**
 	 * Scroll with ofset on page load with hash links in the url
 	 */
-	
-  
+
+
 	/**
 	 * Preloader
 	 */
 	let preloader = select('#preloader');
 	if (preloader) {
-	  window.addEventListener('load', () => {
-		preloader.remove()
-	  });
+		window.addEventListener('load', () => {
+			preloader.remove()
+		});
 	}
-  
-	
-  
+
+
+
 	/**
-	 * Initiate portfolio lightbox 
+	 * Initiate portfolio lightbox
 	 */
 	const portfolioLightbox = GLightbox({
-	  selector: '.portfolio-lightbox'
+		selector: '.portfolio-lightbox'
 	});
-  
+
 	/**
-	 * Initiate portfolio details lightbox 
+	 * Initiate portfolio details lightbox
 	 */
 	const portfolioDetailsLightbox = GLightbox({
-	  selector: '.portfolio-details-lightbox',
-	  width: '90%',
-	  height: '90vh'
+		selector: '.portfolio-details-lightbox',
+		width: '90%',
+		height: '90vh'
 	});
-  
+
 	/**
 	 * Portfolio details slider
 	 */
 	new Swiper('.portfolio-details-slider', {
-	  speed: 400,
-	  loop: true,
-	  autoplay: {
-		delay: 5000,
-		disableOnInteraction: false
-	  },
-	  pagination: {
-		el: '.swiper-pagination',
-		type: 'bullets',
-		clickable: true
-	  }
+		speed: 400,
+		loop: true,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true
+		}
 	});
-  
+
 	/**
 	 * Skills animation
 	 */
 	let skilsContent = select('.skills-content');
 	if (skilsContent) {
-	  new Waypoint({
-		element: skilsContent,
-		offset: '80%',
-		handler: function(direction) {
-		  let progress = select('.progress .progress-bar', true);
-		  progress.forEach((el) => {
-			el.style.width = el.getAttribute('aria-valuenow') + '%'
-		  });
-		}
-	  })
+		new Waypoint({
+			element: skilsContent,
+			offset: '80%',
+			handler: function (direction) {
+				let progress = select('.progress .progress-bar', true);
+				progress.forEach((el) => {
+					el.style.width = el.getAttribute('aria-valuenow') + '%'
+				});
+			}
+		})
 	}
-    
-  })()
+
+})()
